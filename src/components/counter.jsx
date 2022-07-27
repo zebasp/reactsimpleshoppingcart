ea75ab48c711fe27b67265226a533c023bf4f09f
Sebastian <pazseba@hotmail.com>
2022-07-26 23:14:57 -0300

    setting attributes - css class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0
-  }
+  };
 
     render() { 
         return (
           <React.Fragment>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <span className="badge bg-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn.sm">Increment</button>
           </React.Fragment>
         );
     }
