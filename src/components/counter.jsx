9ba1f1002adef2ce9e144d503a421fb6fd85c3eb
Sebastian <pazseba@hotmail.com>
2022-09-03 21:46:04 -0300

    15 updating state - counter filter and passing counter prop

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleIncrement = () => {
@@ -10,8 +10,6 @@ class Counter extends Component {
   };
 
   render() {
-    console.log("props:", this.props);
-
     return (
       <React.Fragment>
         <h4>Counter # {this.props.id}</h4>
@@ -23,7 +21,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
