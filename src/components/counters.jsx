c9adb170ec838eb1842e3e6b05e8d100eff3a536
Sebastian <pazseba@hotmail.com>
2022-09-04 19:28:01 -0300

    16 - reset button styled

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -27,7 +27,7 @@ class Counters extends Component {
   render() {
     return (
       <React.Fragment>
-        <button onClick={this.handleReset} className="btn-primary btn-sm m-2">
+        <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
         {this.state.counters.map((counter) => (
