c36f5ea7b367daff2fa76e5a0084c4194e0225e1
Sebastian <pazseba@hotmail.com>
2022-09-04 14:56:54 -0300

    19 - state lifted up - navbar total counters

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
 class NavBar extends Component {
-
   render() {
     return (
       <nav className="navbar bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            Total{" "}
+            <span className="badge rounded-pill bg-secondary">
+              {this.props.totalCounters}
+            </span>
           </a>
         </div>
       </nav>
